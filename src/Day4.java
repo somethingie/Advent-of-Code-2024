0806261c5731519b0eea86b2aa170ebf6b4819ca
sophierijkers <s.a.j.rijkers@student.utwente.nl>
2024-12-04 20:33:50 +0100

    Day 4, just gotta implement a bit more.

--- src/Day4.java
+++ src/Day4.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
+
 public class Day4 {
 
     Day4PuzzleInput input;
+    String[][] array;
 
     public static void main(String[] args) {
         Day4 today = new Day4();
-
+        today.makeArray();
     }
 
     Day4() {
@@ -38,6 +41,32 @@ public class Day4 {
      * Specifically if A is surrounded by an M and an S,
      * and if they are then we check in front of M for an X
      */
+    private void makeArray() {
+        String[] rows = input.input.split("\n");
+        array = new String[rows.length][];
+        for (int i = 0; i < rows.length ; i++) {
+            array[i] = rows[i].split("");
+            System.out.println(Arrays.toString(array[i]));
+        }
+    }
+
+    private int countXMAS() {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j].equals("M")) {
+                    if (i!=0 && j!=0 && i!=array.length-1 && j!=array[i].length-1) {
+                    //TODO: check around it
+                    }else if ((i == 0 || i == array.length - 1) && (j!=0 || j!=array[i].length - 1)) {
+                        //TODO: check neighbours
+                    } else if ((i != 0 || i != array.length - 1) && (j==0 || j==array[i].length - 1)) {
+                        //TODO: check neighbours
+                    } // else { corners, nothing to find here}
+                }
+            }
+        }
+        return count;
+    }
 
 
 
