e9d1606986f1b68d6b57180cb8770ee37e52f7df
sophierijkers <s.a.j.rijkers@student.utwente.nl>
2024-12-07 12:59:13 +0100

    Day 5 part on, we're going on

--- src/Day5.java
+++ src/Day5.java
@@ -1,17 +1,24 @@
-import java.lang.reflect.Array;
 import java.util.*;
 
 /**
  * We can always sort the requirements into one long list.
+ * 
+ * Dude, I have thought of several ways of doing this:
+ * Try all options per guideline
+ * Try all options per set (Higher or lower)
+ * Make one long list. 
  */
 public class Day5 {
 
-    Set<Tuple> guidelines;
-    List<Integer[]> manual;
+    SetTwoPointO[] guidelines; //preferable it's an array of size 99 with sets in it
+    Integer[] sortedlist;
+    int countCorrectMiddles;
 
     public static void main(String[] args) {
         Day5 today = new Day5();
         today.setUp();
+        System.out.println("1: " + today.countCorrectMiddles);
+//        today.tryToSort();
     }
 
     private Day5() {
@@ -19,92 +26,98 @@ public class Day5 {
 
 
     private void setUp() {
-        String puzzleInput = (new Day5PuzzleInput("small")).input;
+        String puzzleInput = (new Day5PuzzleInput("big")).input;
         String[] split = puzzleInput.split("\n");
-        boolean page = false;
-        int ind = 0;
-        manual = new ArrayList<Integer[]>();
+        sortedlist = new Integer[99];
+
+        boolean page = false; //To see if the function of the page changed
+
+        countCorrectMiddles = 0;
+
+        guidelines = new SetTwoPointO[99];
+
+        for (int i = 0; i < 99; i++) {
+            guidelines[i] = new SetTwoPointO();
+        }
+
         for (int i = 0; i < split.length; i++) {
-            String line = split[i];
-            if (line.trim().isEmpty()) {
-                ind = i;
+            if (split[i].trim().isEmpty()) {
                 page = true;
             } else if (!page) {
-                String[] pages = line.split("\\|");
-                guidelines = new HashSet<Tuple>();
-                guidelines.add(new Tuple(Integer.parseInt(pages[0]), Integer.parseInt(pages[1])));;
+                String[] pages = split[i].split("\\|");
+                // Put the second value in the after of the first value
+                guidelines[Integer.parseInt(pages[0])].after.add(Integer.parseInt(pages[1]));
+                // Put the first value in the before of the second value
+                guidelines[Integer.parseInt(pages[1])].before.add(Integer.parseInt(pages[0]));
             } else {
-                String[] pages = line.split(",");
-                Integer[] index = new Integer[pages.length];
+                String[] pages = split[i].split(",");
+                Integer[] manual = new Integer[pages.length];
                 for (int j = 0; j < pages.length; j++) {
-                    index[j] = Integer.parseInt(pages[j]);
+                    manual[j] = Integer.parseInt(pages[j]);
                 }
-                manual.add(index);
+                if (seeIfValid(manual)) {
+                    countCorrectMiddles += getMiddleValue(manual);
+                } else {
+                    //cry
+                    //or Sort them and then count middle.
+                }
             }
         }
     }
 
-    private int countmiddles() {
-        int index = getIndex(0);
-        int count = 0;
-        while (index != -1) {
-            //get middle
-            //count up middle
-            index = getIndex(index);
-        }
-        return count;
-    }
+//    private void tryToSort(Integer[] toSort) {
+//
+//    }
 
     /**
-     * Returns the index of the correct lists in manuel. It does this by comparing them to the Set guidelines.
-     * @param begin
-     * @return
+     * [75,47,61,53,29]
+     * Set<Integer> pagesAfter = {61, 53, 29}
+     * Set<Integer> pagesBefore = {75}
+     * check if invalid.
+     *
+     * @param manual array of the manual thing
+     * @return true if valid
      */
-    private int getIndex(int begin) {
+    private boolean seeIfValid(Integer[] manual) {
+        for (int i = 0; i < manual.length; i++) { //for every integer in the array, check:
+            Integer[] after = Arrays.copyOfRange(manual, i + 1, manual.length); //all the numbers that come after it
+            Set<Integer> pagesAfter = new HashSet<>(Arrays.asList(after));
 
-        return -1;
+            ; // All the values that should be before it
+//            System.out.println(manual[i] + " Before: " + Arrays.toString(guidelines[manual[i]].before.toArray()) + " After: " + Arrays.toString(guidelines[manual[i]].after.toArray()));
+            if (!Collections.disjoint(pagesAfter, guidelines[manual[i]].before)) {
+                return false;
+            }
+
+            Integer[] before = Arrays.copyOfRange(manual, 0, i + 1);
+            Set<Integer> pagesBefore = new HashSet<>(Arrays.asList(before));
+
+            if (!Collections.disjoint(pagesBefore, guidelines[manual[i]].after)) {
+                return false;
+            }
+        }
+        return true;
     }
 
-
-
-
-    class Tuple {
-        int first;
-        int second;
-
-        public Tuple(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-
-        public int getFirst() {
-            return first;
-        }
-
-        public int getSecond() {
-            return second;
-        }
+    private int getMiddleValue(Integer[] manual) {
+        return manual[manual.length / 2];
     }
+
+    private int getCountCorrectMiddles() {
+        return countCorrectMiddles;
+    }
+
 
     /**
      * A class that returns all integers mentioned to be before or after the chosen integer.
      */
-    class BeforeAfter {
-        Integer[] before;
-        Integer[] after;
+    class SetTwoPointO {
+        public Set<Integer> before;
+        public Set<Integer> after;
 
-        public BeforeAfter() {
-            for (int i = 0; i < ; i++) {
-
-            }
-        }
-
-        public Integer[] getBefore() {
-            return before;
-        }
-
-        public Integer[] getAfter() {
-            return after;
+        public SetTwoPointO() {
+            before = new HashSet<>();
+            after = new HashSet<>();
         }
     }
 
